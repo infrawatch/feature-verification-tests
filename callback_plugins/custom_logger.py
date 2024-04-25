4cc570664f1baeb17f19e89c629518d7f3e2dec6
mgirgisf <mgirgisf@redhat.com>
2024-04-25 13:39:46 +0200

    Update callback_plugin

--- callback_plugins/custom_logger.py
+++ callback_plugins/custom_logger.py
@@ -1,72 +1,65 @@
-from __future__ import (absolute_import, division, print_function)
-#__metaclass__ = type
-
+from ansible.plugins.callback import CallbackBase
 import os
 
-from ansible.plugins.callback import CallbackBase
-
-DOCUMENTATION = '''
-    callback: log_to_file
-    type: notification
-    short_description: output logs to a file
-    description:
-        - This summarises tasks per host and outputs it to a log file
-'''
-
 class CallbackModule(CallbackBase):
-    """
-    logs playbook results, per host, in /var/log/ansible/hosts
-    """
+
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'notification'
     CALLBACK_NAME = 'log_to_file'
     CALLBACK_NEEDS_WHITELIST = True
 
-    MSG_FORMAT = "{task}={status}\n"
-
     def __init__(self):
-
         super(CallbackModule, self).__init__()
-        self.current_task = ''
-        self.passed = 0
-        self.failed = 0
         self.output_dir = os.getcwd()
-        self.path = os.path.join(self.output_dir, "test_run_result.out")
-        # either create the file or truncate the existing file
-        with open(self.path, 'w') as fp:
-            pass
+        self.results = {}
 
-    def playbook_on_task_start(self, task, is_conditional):
-        self.current_task = str(task)
+    def playbook_on_stats(self, stats):
+        # Log results for each host
+        for host in stats.processed:
+            summary = stats.summarize(host)
+            self.results[host] = summary
+            self._log_results(host, summary)
 
-#    def playbook_on_stats(self, stats):
-#        with open(self.path, 'a') as fd:
-#            fd.write("passed: {}\n".format(self.passed))
-#            fd.write("failed: {}\n".format(self.failed))
+    def _log_results(self, host, summary):
+        file_path = os.path.join(self.output_dir, f"test_run_results.log")
+        with open(file_path, 'w') as f:
+            f.write(f"Host: {host}\n")
+            f.write(f"Tasks Succeeded: {summary['ok']}\n")
+            f.write(f"Tasks Failed: {summary['failures']}\n")
+            f.write(f"Tasks Skipped: {summary['skipped']}\n")
+            if 'failed_task_names' in self.results[host]:
+                f.write("Failed Tasks:\n")
+                for task_name in self.results[host]['failed_task_names']:
+                    f.write(f"  - {task_name}\n")
+            if 'ok_task_names' in self.results[host]:
+                f.write("Succeeded Tasks:\n")
+                for task_name in self.results[host]['ok_task_names']:
+                    f.write(f"  - {task_name}\n")
 
-    def log(self, host, category):
-         # only interested in the test tasks, not setup or debug
-         if self.current_task.startswith("RHELOSP"):
+    def v2_runner_on_ok(self, result):
+        host = result._host
+        task_name = result.current_task
+        self._log_task_result(host, 'ok', task_name)
 
-             if category == "failed":
-                self.failed +=1
-             if category == "passed":
-                 self.passed +=1
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        host = result._host.get_name()
+        task_name = result._task.get_name()
+        self._log_task_result(host, 'failed', task_name)
 
-             test_result_message = self.MSG_FORMAT.format(task=self.current_task, status=category)
+    def v2_runner_on_skipped(self, result):
+        host = result._host.get_name()
+        task_name = result._task.get_name()
+        self._log_task_result(host, 'skipped', task_name)
 
-             with open(self.path, 'a') as fd:
-                 fd.write(test_result_message)
-
-             file_name = "_".join(["test_run_result", host])
-             with open(os.path.join(self.output_dir, file_name ) , 'a') as fd:
-                 fd.write(test_result_message)
-
-    def runner_on_failed(self, host, res, ignore_errors=False):
-         self.log(host,'failed')
-
-    def runner_on_ok(self, host, res):
-         self.log(host,'passed')
-
-    def runner_on_skipped(self, host, res, item=None):
-         self.log(host, 'skipped')
+    def _log_task_result(self, host, result, task_name):
+        if host not in self.results:
+            self.results[host] = {'ok': 0, 'failures': 0, 'skipped': 0}
+        if result == 'failed':
+            if 'failed_task_names' not in self.results[host]:
+                self.results[host]['failed_task_names'] = []
+            self.results[host]['failed_task_names'].append(task_name)
+        elif result == 'ok':
+            if 'ok_task_names' not in self.results[host]:
+                self.results[host]['ok_task_names'] = []
+            self.results[host]['ok_task_names'].append(task_name)
+        self.results[host][result] += 1
