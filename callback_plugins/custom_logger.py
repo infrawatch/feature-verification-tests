7443e79ede9e911a93e01291fd1a95579f7b56bf
mgirgisf <mgirgisf@redhat.com>
2024-07-22 08:41:12 +0300

    update host to run on controller

--- callback_plugins/custom_logger.py
+++ callback_plugins/custom_logger.py
@@ -31,7 +31,7 @@ class CallbackModule(CallbackBase):
 
     def __init__(self):
         super(CallbackModule, self).__init__()
-        self.output_dir = "/tmp"
+        self.output_dir = os.getcwd()
         self.results = {}
         
     def playbook_on_stats(self, stats):
