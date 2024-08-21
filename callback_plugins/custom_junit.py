e6975c899b51073228d5faabc892347c177db8f9
Emma Foley <efoley@redhat.com>
2024-08-21 13:39:06 +0100

    [debug] Add print statements to the mutate_task_name to show task name change

--- callback_plugins/custom_junit.py
+++ callback_plugins/custom_junit.py
@@ -59,18 +59,32 @@ class CallbackModule(JunitCallbackModule):
     #         task_data.add_host(HostData(host_uuid, host_name, status, result))
 
     def mutate_task_name(self, task_name):
+
+        print("enter mutate_task_name")
+
         new_name = task_name
+        print(new_name)
         new_name = new_name.split("\n")[0]  # only use the first line, so we can include IDs and additional description
+        print(new_name)
+        print("taskname (original): %s" % task_name)
+
         new_name = new_name.split(":")[-1]  # only provide the last part of the name when the role name is included
 
         new_name = re.sub(r'^.*?\S*%s\S*' % (self._test_case_prefix), '', new_name)  # remove the test prefix and everything before it
+        print("%s\t(remove test prefix)" % new_name)
 
         new_name = new_name.lower()
+        print("%s\t(lowercase)" % new_name)
 
         new_name = re.sub(r'\W', ' ', new_name)  # replace all non-alphanumeric characters (except _) with a space
+        print("%s (Replace non-alphanumerics with a space)" % new_name)
         new_name = re.sub(r'(^\W*|\W*$)', '', new_name)  # trim any trailing or leading non-alphanumeric characters
+        print(new_name)
         new_name = re.sub(r' +', '_', new_name)  # replace any number of spaces with _
+        print("%s\t(spaces -> _)" % new_name)
 
+        print(new_name == task_name)
+        print("exit mutate_task_name")
         return new_name
 
     def _build_test_case(self, task_data, host_data):
@@ -78,6 +92,7 @@ class CallbackModule(JunitCallbackModule):
            This is used in generate_report. The task_data and host data will get passed.
         """
         ## I want to test updating the name after the super class has done its thing
+        print("What is the task name prior to running super?\n\t%s" % task_data.name)
 
         # Use the original task name to define the final name
         new_name = self.mutate_task_name(task_data.name)
@@ -85,6 +100,7 @@ class CallbackModule(JunitCallbackModule):
         tc = super()._build_test_case(task_data, host_data)
 
         tc.name = self.mutate_task_name(task_data.name)
+        print("%s\t(tc.name)" % tc.name)
 
         # I don't want these properties for now; I may be able to omit them with a config option
         tc.system_out = None
