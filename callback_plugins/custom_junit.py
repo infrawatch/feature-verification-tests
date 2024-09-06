a84d458244cdcdeb5237380a71738d470dc2c81c
Emma Foley <efoley@redhat.com>
2024-09-06 19:04:55 +0100

    Get more info from custom_junit

--- callback_plugins/custom_junit.py
+++ callback_plugins/custom_junit.py
@@ -20,8 +20,8 @@ class CallbackModule(JunitCallbackModule):
         # self._output_dir = os.getcwd()
         # Update this to parse these values from the config file, as well as the env.
         self._output_dir = os.path.expanduser("~/.ansible.log")
-        #self._test_case_prefix = os.getenv('JUNIT_TEST_CASE_PREFIX', '[TEST]')
-        self._test_case_prefix = os.getenv('JUNIT_TEST_CASE_PREFIX', '')
+        self._test_case_prefix = os.getenv('JUNIT_TEST_CASE_PREFIX', '[TEST]')
+        # self._test_case_prefix = os.getenv('JUNIT_TEST_CASE_PREFIX', '')
         #self._fail_on_ignore = os.getenv('JUNIT_FAIL_ON_IGNORE', 'False').lower()
         self._fail_on_ignore = 'true'  # this is needed because we use "ignore_errors" on the playbooks so that all the tests are run
         self._include_setup_tasks_in_report = os.getenv('JUNIT_INCLUDE_SETUP_TASKS_IN_REPORT', 'False').lower()
@@ -34,15 +34,12 @@ class CallbackModule(JunitCallbackModule):
             print("Creating output dir: %s" % (self._output_dir))
             os.makedirs(self._output_dir)
 
-    # Don't need this. since we're going to be filtering on the [TEST] prefix (or similar), which will be at the start of the taskname
-    # If the prefix is at the start of the name, we can filter on that, and remove anything that comes before it (e.g. if a tasks comes from an included role, then "role_name :" is prefixed
     def _finish_task(self, status, result):
-        """
-        Custom finish task method
-        """
-        super(CallbackModule, self)._finish_task(status, result)
+        """ record the results of a task for a single host """
+        # TODO: try enclosing all this in "if _prefix in name", since we only want to record the results with a prefix
+        # this could then bevome "if prefix in task name: super()"
         task_uuid = result._task._uuid
-        task_data = self._task_data[task_uuid]
+
         if hasattr(result, '_host'):
             host_uuid = result._host._uuid
             host_name = result._host.name
@@ -50,8 +47,30 @@ class CallbackModule(JunitCallbackModule):
             host_uuid = 'include'
             host_name = 'include'
 
-        if self._test_case_prefix in task_data.name:
+        task_data = self._task_data[task_uuid]
+
+        if self._fail_on_change == 'true' and status == 'ok' and result._result.get('changed', False):
+            status = 'failed'
+
+        # ignore failure if expected and toggle result if asked for
+        if status == 'failed' and 'EXPECTED FAILURE' in task_data.name:
+            status = 'ok'
+        elif 'TOGGLE RESULT' in task_data.name:
+            if status == 'failed':
+                status = 'ok'
+            elif status == 'ok':
+                status = 'failed'
+
+        if task_data.name.startswith(self._test_case_prefix) or status == 'failed':
+            # This works, but adding the update here means re-implementing the whole _finish_task method.
+            # task_data.name = self.mutate_task_name(task_data.name)
             task_data.add_host(HostData(host_uuid, host_name, status, result))
+
+        # This bit is new
+        if task_data.name.startswith(self._test_case_prefix):
+            print(f"This task ({task_data.name}) should be reported becasue it contains test_prefix({self._test_case_prefix}")
+        if status == 'failed':
+            print(f"This task ({task_data.name}) should be reported because it failed")
 
     def mutate_task_name(self, task_name):
         print("enter mutate_task_name(task_name=%s)" % task_name)
@@ -64,7 +83,7 @@ class CallbackModule(JunitCallbackModule):
         new_name = new_name.split("\n")[0]  # only use the first line, so we can include IDs and additional description
         print("%s\t(take the first line of the task name)" % new_name)
 
-        # this cover when a task is included, but the including task is the one that is the test
+        # this covers when a task is included, but the including task is the one that is the test
         new_name = new_name.split(":")[-1]  # only provide the last part of the name when the role name is included
         print("%s\t(split at :, take last element)" % new_name)
 
@@ -93,18 +112,20 @@ class CallbackModule(JunitCallbackModule):
            This is used in generate_report. The task_data and host data will get passed.
         """
         # Use the original task name to define the final name
-        # new_name = self.mutate_task_name(task_data.name)
+        #new_name = self.mutate_task_name(task_data.name)
 
+        print("%s\t(tc.name, pre-_build_test_case)" % tc.name)
         tc = super()._build_test_case(task_data, host_data)
         print("%s\t(tc.name, post-_build_test_case)" % tc.name)
-        # tc.name = new_name
         tc.name = self.mutate_task_name(tc.name)
-        #tc.name = task_data.name
 
-        print("%s\t(tc.name)" % tc.name)
+        print("%s\t(tc.name, post-mutate_task_name)" % tc.name)
 
         # I don't want these properties for now; I may be able to omit them with a config option
+        # These two control whether testcases contain the system_out and
+        # system_err elements that show STDOUT and STDERR
         tc.system_out = None
         tc.system_err = None
+
         tc.classname = "openstack-observability"
         return tc
