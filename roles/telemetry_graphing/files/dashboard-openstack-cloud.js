f100d60c44b8153da8a473f053d6899a9947bb95
mgirgisf <mgirgisf@redhat.com>
2025-06-05 09:41:30 +0200

    debug

--- roles/telemetry_graphing/files/dashboard-openstack-cloud.js
+++ roles/telemetry_graphing/files/dashboard-openstack-cloud.js
@@ -4,27 +4,23 @@ describe('OpenShift Console Login', () => {
     const openshiftUrl = Cypress.env('OPENSHIFT_URL') || 'https://console-openshift-console.apps-crc.testing';
     const username = Cypress.env('OPENSHIFT_USERNAME') || 'kubeadmin';
     const password = Cypress.env('OPENSHIFT_PASSWORD') || '12345678';
+    const provider = 'kube:admin';
 
     // Visit the OpenShift console
     cy.visit(openshiftUrl);
 
-    // Wait for the login page to load
-    cy.url().should('include', 'oauth');
 
-    cy.get('input[id="inputUsername"]').invoke('val', username).trigger('input');
-    cy.get('input[id="inputPassword"]').invoke('val', password).trigger('input');
-    cy.get('input[id="inputUsername"]').should('be.visible').type(username, { delay: 50 , force: true });
-    cy.get('input[id="inputPassword"]').should('be.visible').type(password, { delay: 50, force: true });
-
-    cy.get('#inputUsername').then($input => {
-      const input = $input[0];
-      input.value = username;
-      input.dispatchEvent(new Event('input', { bubbles: true }));
-      input.dispatchEvent(new Event('change', { bubbles: true }));
+    cy.task('log', `  Logging in as ${username}`);
+    cy.get('[data-test-id="login"]').should('be.visible');
+    cy.get('body').then(($body) => {
+      if ($body.text().includes(provider)) {
+        cy.contains(provider).should('be.visible').click();
+      }
     });
+    cy.get('#inputUsername').type(username);
+    cy.get('#inputPassword').type(password);
+    cy.get('button[type=submit]').click();
 
-    // Submit the login form
-    cy.get('button[type="submit"]').click();
 
     // Wait for successful login and redirect to console
     cy.url({ timeout: 15000 }).should('include', 'console-openshift-console');
