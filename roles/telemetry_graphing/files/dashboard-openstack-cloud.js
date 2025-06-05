5958f8b63e2bea24577cf8a0fdb6c1441ff71167
mgirgisf <mgirgisf@redhat.com>
2025-06-05 12:48:32 +0200

    debug

--- roles/telemetry_graphing/files/dashboard-openstack-cloud.js
+++ roles/telemetry_graphing/files/dashboard-openstack-cloud.js
@@ -5,12 +5,19 @@ describe('OpenShift Console Login', () => {
     const username = Cypress.env('OPENSHIFT_USERNAME') || 'kubeadmin';
     const password = Cypress.env('OPENSHIFT_PASSWORD') || '12345678';
     const provider = 'kube:admin';
+    const dashboards = [
+      { url: '/grafana-dashboard-openstack-cloud', screenshot: 'openstack-cluster' },
+      { url: '/grafana-dashboard-openstack-rabbitmq', screenshot: 'openstack-rabbitmq' },
+      { url: '/grafana-dashboard-openstack-node', screenshot: 'openstack-node' },
+      { url: '/grafana-dashboard-openstack-vm', screenshot: 'openstack-vms' },
+      { url: '/grafana-dashboard-openstack-network-traffic', screenshot: 'openstack-network-traffic'},
+      //{ url: '/grafana-dashboard-openstack-kepler', screenshot: 'openstack-kepler'},
+      //{ url: '/grafana-dashboard-openstack-ceilometer-ipmi', screenshot: 'openstack-ceilometer-ipmi' }
+    ];
 
     // Visit the OpenShift console
     cy.visit(openshiftUrl);
 
-
-    cy.task('log', `  Logging in as ${username}`);
     cy.get('[data-test-id="login"]').should('be.visible');
     cy.get('body').then(($body) => {
       if ($body.text().includes(provider)) {
@@ -25,5 +32,16 @@ describe('OpenShift Console Login', () => {
     // Wait for successful login and redirect to console
     cy.url({ timeout: 15000 }).should('include', 'console-openshift-console');
     cy.screenshot("login");
+
+    dashboards.forEach(dashboard => {
+      cy.visit(`https://console-openshift-console.apps-crc.testing/monitoring/dashboards${dashboard.url}`);
+
+      // Wait for the dashboard to load and take a screenshot
+      cy.get('div[data-test-id="dashboard"]', { timeout: 100000 })
+        .find('[data-test-id^="panel-"]')
+
+      cy.wait(5000);
+      cy.screenshot(dashboard.screenshot);
+    });
   });
 });
