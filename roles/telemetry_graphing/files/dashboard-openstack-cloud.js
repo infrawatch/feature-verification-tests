774dbd959511dda774c4516fedd3f234618a172c
mgirgisf <mgirgisf@redhat.com>
2025-06-06 09:18:32 +0200

    debug

--- roles/telemetry_graphing/files/dashboard-openstack-cloud.js
+++ roles/telemetry_graphing/files/dashboard-openstack-cloud.js
@@ -1,47 +1,38 @@
 describe('OpenShift Console Login', () => {
 
-  it('should successfully login to OpenShift console', () => {
-    const openshiftUrl = Cypress.env('OPENSHIFT_URL') || 'https://console-openshift-console.apps-crc.testing';
+  it('should successfully login to OpenShift console', () => {;
     const username = Cypress.env('OPENSHIFT_USERNAME') || 'kubeadmin';
     const password = Cypress.env('OPENSHIFT_PASSWORD') || '12345678';
     const provider = 'kube:admin';
-    const dashboards = [
-      { url: '/grafana-dashboard-openstack-cloud', screenshot: 'openstack-cluster' },
-      { url: '/grafana-dashboard-openstack-rabbitmq', screenshot: 'openstack-rabbitmq' },
-      { url: '/grafana-dashboard-openstack-node', screenshot: 'openstack-node' },
-      { url: '/grafana-dashboard-openstack-vm', screenshot: 'openstack-vms' },
-      { url: '/grafana-dashboard-openstack-network-traffic', screenshot: 'openstack-network-traffic'},
-      //{ url: '/grafana-dashboard-openstack-kepler', screenshot: 'openstack-kepler'},
-      //{ url: '/grafana-dashboard-openstack-ceilometer-ipmi', screenshot: 'openstack-ceilometer-ipmi' }
-    ];
 
     // Visit the OpenShift console
-    cy.visit(openshiftUrl);
+    cy.visit(Cypress.config('baseUrl'));
 
-    cy.get('[data-test-id="login"]').should('be.visible');
-    cy.get('body').then(($body) => {
-      if ($body.text().includes(provider)) {
-        cy.contains(provider).should('be.visible').click();
-      }
-    });
-    cy.get('#inputUsername').type(username);
-    cy.get('#inputPassword').type(password);
-    cy.get('button[type=submit]').click();
+    cy.url().should('include', 'oauth-openshift.apps-crc.testing');
+
+    cy.origin(
+      'oauth-openshift.apps-crc.testing',
+      { args: { provider, username, password } },
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      ({ provider, username, password }) => {
+        cy.task('log', `  Logging in as ${username}`);
+        cy.get('[data-test-id="login"]').should('be.visible');
+        cy.get('body').then(($body) => {
+          if ($body.text().includes(provider)) {
+            cy.contains(provider).should('be.visible').click();
+          }
+        });
+        cy.get('#inputUsername').type(username);
+        cy.get('#inputPassword').type(password);
+        cy.get('button[type=submit]').click();
+      },
+    );
 
 
+    cy.visit(Cypress.config('baseUrl'));
     // Wait for successful login and redirect to console
-    cy.url({ timeout: 15000 }).should('include', 'console-openshift-console');
+
     cy.screenshot("login");
 
-    dashboards.forEach(dashboard => {
-      cy.visit(`https://console-openshift-console.apps-crc.testing/monitoring/dashboards${dashboard.url}`);
-
-      // Wait for the dashboard to load and take a screenshot
-      cy.get('div[data-test-id="dashboard"]', { timeout: 100000 })
-        .find('[data-test-id^="panel-"]')
-
-      cy.wait(5000);
-      cy.screenshot(dashboard.screenshot);
-    });
   });
 });
