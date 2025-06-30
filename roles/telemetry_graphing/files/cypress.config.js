1a3d8c32880056f25413078cd1b6ae366b0ba724
Marihan Girgis <102027102+mgirgisf@users.noreply.github.com>
2025-06-30 10:48:03 +0200

    [Graphing Job] Enable the monitoring (#280)
    * enable crc monitoring

--- roles/telemetry_graphing/files/cypress.config.js
+++ roles/telemetry_graphing/files/cypress.config.js
@@ -4,5 +4,11 @@ module.exports = defineConfig({
     baseUrl: 'https://console-openshift-console.apps-crc.testing/login',
     specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
     supportFile: false,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 30000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
   },
 })
