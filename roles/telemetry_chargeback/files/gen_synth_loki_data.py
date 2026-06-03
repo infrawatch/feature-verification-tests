1533e63149569c50d2759defb4852fb82d289050
ayefimov <ayefimov@redhat.com>
2026-06-03 17:19:40 -0400

    Update role to generate synthetic data for each scenario that does not overlap

--- roles/telemetry_chargeback/files/gen_synth_loki_data.py
+++ roles/telemetry_chargeback/files/gen_synth_loki_data.py
@@ -446,7 +446,7 @@ def main():
         "--descending",
         action="store_true",
         dest="reverse",
-        default=True,
+        default=False,
         help="Sort timestamps in descending order: newest first, oldest last "
              "(default behavior)."
     )
